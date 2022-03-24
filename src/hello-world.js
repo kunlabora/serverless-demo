3d9d1c9eef6bdaa3f0f5d4c2223029364fc9c637
dieter.vanpeer@kunlabora.be <dieter-kunlabora>
2022-03-24 14:41:03 +0100

    Hello name

--- src/hello-world.js
+++ src/hello-world.js
@@ -1,8 +1,11 @@
 // Documentation: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-handler.html
 
 exports.handler = async function (event, context) {
+
+    const name = event.queryStringParameters?.name ?? 'World';
+
     return {
         statusCode: 200,
-        body: JSON.stringify({message: 'Hello World!'}),
+        body: JSON.stringify({message: `Hello ${name}!`}),
     };
 };
